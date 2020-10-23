d18c87fec4877e06db0dedc48a5c144541e21485
msis <mohammadshahrulizham@gmail.com>
2020-10-24 02:38:09 +0800

    test commit

--- ai-engine/ai/engine.py
+++ ai-engine/ai/engine.py
@@ -1,4 +1,4 @@
-# engine ai disini
+# engine ai disinilah semuanya menjadi
 import cv2
 import os
 import numpy as np
@@ -10,17 +10,19 @@ import datetime
 import pika
 import json
 
-#-------------------------------
+# -------------------------------
 # Setup mongo configuration
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["skymind"]
 mycol_employee = mydb["employee"]
 mycol_attendance = mydb["attendance"]
-#-------------------------------
+
+
+# -------------------------------
 
 def message(emp_num):
     date = str(datetime.datetime.now().date())
-    dt_obj = datetime.datetime.strptime(date,'%Y-%m-%d')
+    dt_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
     date = str(int(dt_obj.timestamp() * 1000))
     print(date)
 
@@ -28,11 +30,11 @@ def message(emp_num):
     print(time)
 
     x = {
-            "emp_num": emp_num,
-            "date" : date,
-            "time_in": time,
-            "time_out": time
-        }
+        "emp_num": emp_num,
+        "date": date,
+        "time_in": time,
+        "time_out": time
+    }
 
     # convert into JSON:
     y = json.dumps(x)
@@ -41,8 +43,8 @@ def message(emp_num):
 
     return y
 
-def rabbit_sender(messages):
 
+def rabbit_sender(messages):
     # establish the connection
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     attendance = connection.channel()
@@ -67,8 +69,8 @@ def rabbit_sender(messages):
     # rabbit_receiver()
     # return {rabbit-receiver : rabbit should receive emp_num from java application}
 
-def capture_data(haar_cascade_path, img_dataset, database_path):
 
+def capture_data(haar_cascade_path, img_dataset, database_path):
     # take image from the camera
     cam = cv2.VideoCapture(0)
 
@@ -76,8 +78,8 @@ def capture_data(haar_cascade_path, img_dataset, database_path):
     # print(cam.get(cv2.CAP_PROP_FPS))
 
     # minimum size to be recognized as a face
-    minW = int(cam.get(3)/3) # width of the picture
-    minH = int(cam.get(4)/3) # height of the picture
+    minW = int(cam.get(3) / 3)  # width of the picture
+    minH = int(cam.get(4) / 3)  # height of the picture
 
     # maximum sample to be taken
     max_sample = 30
@@ -89,7 +91,7 @@ def capture_data(haar_cascade_path, img_dataset, database_path):
     emp_num = int(input('\n Enter Employee Number : '))
 
     # try to query employee number requireed
-    myquery = { "emp_num": emp_num}
+    myquery = {"emp_num": emp_num}
     mydoc = mycol_employee.find(myquery)
 
     # initialize database = 0, later if query cannot found the employee
@@ -105,11 +107,11 @@ def capture_data(haar_cascade_path, img_dataset, database_path):
 
     # if the employee number are registred in database then
     # the image capturing process will begin
-    if(emp_num == database):
+    if (emp_num == database):
         print("\n Please look at the camera and wait.....")
 
         # using while loop in order to extract the image frame one by one
-        while(True):
+        while (True):
             # Take image frame one by one
             ret, img = cam.read()
 
@@ -119,40 +121,39 @@ def capture_data(haar_cascade_path, img_dataset, database_path):
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # detect face
-            faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize = (minW, minH))
+            faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize=(minW, minH))
 
             # using for loop to extract (x,y,w,h) from faces
-            for (x,y,w,h) in faces:
-
+            for (x, y, w, h) in faces:
                 # to calculate the progress of image that has been captured
                 count += 1
-                counter = count/100*100
+                counter = count / 100 * 100
 
                 # put the rectangle on the detected faces
-                cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
                 # cv2.putText(img, str(counter)+"%", (x+5,y+h-5), font, 1, (255,255,0), 2)
-                cv2.putText(img, str("{:.2f} %".format(counter)), (x+5,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 2)
+                cv2.putText(img, str("{:.2f} %".format(counter)), (x + 5, y + h - 5), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (255, 255, 0), 2)
 
                 # Save the captured image into the datasets folder
-                cv2.imwrite(img_dataset + str(emp_num) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
-
+                cv2.imwrite(img_dataset + str(emp_num) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
 
             # To make the window is sizeable
             cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
             # To make window open full screen without titlebar
-            cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
+            cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
             # show the image and rectangle of detected faces
             cv2.imshow('image', img)
 
-            k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+            k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
             if k == 27:
                 cam.release()
                 cv2.destroyAllWindows()
                 break
-            elif count >= max_sample: # Take 30 face sample and stop video
+            elif count >= max_sample:  # Take 30 face sample and stop video
                 cam.release()
                 cv2.destroyAllWindows()
                 break
@@ -160,8 +161,8 @@ def capture_data(haar_cascade_path, img_dataset, database_path):
     else:
         print("Employee number does not exist")
 
-def train_data(haar_cascade_path, img_dataset, trained_model):
 
+def train_data(haar_cascade_path, img_dataset, trained_model):
     # font to be used at rectangle
     font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -173,18 +174,17 @@ def train_data(haar_cascade_path, img_dataset, trained_model):
     # 3. Path for LBPH pretrained model
     face_recognition = cv2.face.LBPHFaceRecognizer_create()
 
-
     print("Please wait for model to train")
 
-    img_datasets = [os.path.join(img_dataset,f) for f in os.listdir(img_dataset)]
+    img_datasets = [os.path.join(img_dataset, f) for f in os.listdir(img_dataset)]
 
     # empty array for face samples
-    face_sample=[]
+    face_sample = []
 
     # empy array for ids
     emp_num = []
 
-    count_sample=0
+    count_sample = 0
     for image in img_datasets:
 
         # Read data in grayscale mode
@@ -194,23 +194,23 @@ def train_data(haar_cascade_path, img_dataset, trained_model):
         id = int(os.path.split(image)[-1].split(".")[0])
         # print(id)
         # faces = face_detection.detectMultiScale(gray)
-        faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize = (213, 160))
+        faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize=(213, 160))
 
-
-        for (x,y,w,h) in faces:
-            #LBPH
-            face_sample.append(gray[y:y+h,x:x+w])
+        for (x, y, w, h) in faces:
+            # LBPH
+            face_sample.append(gray[y:y + h, x:x + w])
             emp_num.append(id)
 
         count_sample += 1
-        progress = count_sample/len(img_datasets)*100
+        progress = count_sample / len(img_datasets) * 100
         print("progress to train {:.2f} %".format(progress))
-        #-------------------------------------
+        # -------------------------------------
     face_recognition.train(face_sample, np.array(emp_num))
     # Save the model into trainer/trainer.yml
-    face_recognition.save(trained_model) # recognizer.save() worked on Mac, but not on Pi #asal recognizer.write
+    face_recognition.save(trained_model)  # recognizer.save() worked on Mac, but not on Pi #asal recognizer.write
     # Print the numer of faces trained and end program
     print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(emp_num))))
+
 
 def recognize(haar_cascade_path, trained_model, datebase_path):
     # take image from the camera
@@ -222,8 +222,8 @@ def recognize(haar_cascade_path, trained_model, datebase_path):
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     # MINIMUM SIZE TO BE RECOGNIZED AS A FACE
-    minW = int(cam.get(3)/3)
-    minH = int(cam.get(4)/3)
+    minW = int(cam.get(3) / 3)
+    minH = int(cam.get(4) / 3)
 
     home = str(Path.home())
     face_detection = cv2.CascadeClassifier(haar_cascade_path)
@@ -243,25 +243,25 @@ def recognize(haar_cascade_path, trained_model, datebase_path):
 
     while True:
         start_time = time.time()  # start time of the loop
-        ret, img =cam.read()
+        ret, img = cam.read()
         #     img = cv2.flip(img, -1) # Flip vertically
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize = (minW, minH))
+        faces = face_detection.detectMultiScale(gray, 1.2, 5, minSize=(minW, minH))
         # emp_name = "aba"
 
         # this is timer for cases where it does not detect the faces
         result = 1.0 / (time.time() - start_time)
         strs = "FPS : {:.0f}".format(result)
 
-        for(x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            #LPBH
-            emp_num, confidence = face_recognition.predict(gray[y:y+h,x:x+w])
+            # LPBH
+            emp_num, confidence = face_recognition.predict(gray[y:y + h, x:x + w])
 
             # find the row of employee number (eg. = 7)
-            emp_name = database.loc[database['Employee Number']==emp_num]
+            emp_name = database.loc[database['Employee Number'] == emp_num]
 
             # after found employee number show the name of the employee by taking
             # the -name- column
@@ -283,8 +283,8 @@ def recognize(haar_cascade_path, trained_model, datebase_path):
                 emp_name = "unknown"
 
             # cv2.putText(img, 'Izham Pass : Door Opens', (10,40), font, 1, (255,255,255), 2)
-            cv2.putText(img, str(emp_name), (x,y-5), font, 1, (255,255,255), 2)
-            cv2.putText(img, "{} %".format(str(acc)), (x+5,y+h-5), font, 1, (255,255,0), 1)
+            cv2.putText(img, str(emp_name), (x, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, "{} %".format(str(acc)), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
             result = 1.0 / (time.time() - start_time)
             strs = "FPS : {:.0f}".format(result)  # FPS = 1 / time to process loop
@@ -296,26 +296,25 @@ def recognize(haar_cascade_path, trained_model, datebase_path):
 
         cv2.namedWindow('image', cv2.WINDOW_NORMAL)
         # cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
-        cv2.imshow('image',img)
+        cv2.imshow('image', img)
 
-
-
-        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
         if k == 27:
             cam.release()
             cv2.destroyAllWindows()
             break
 
+
 # show popup to choose whether to capture/train new data or to recognize
-while(True):
+while (True):
     # to know machine path
     # home = str(Path.home())
     home = "/data/Gudang-Ilmu/Ubuntu-Home/Documents/Github/original/ai-engine"
 
-    haar_cascade_path = home+'/haarcascade_frontalface_default.xml'
-    trained_model = home+'/trainer.yml'
-    img_dataset = home+'/dataset/'
-    database_path = home+"/face_database.csv"
+    haar_cascade_path = home + '/haarcascade_frontalface_default.xml'
+    trained_model = home + '/trainer.yml'
+    img_dataset = home + '/dataset/'
+    database_path = home + "/face_database.csv"
 
     try:
         choice = int(input("\n1. Capture data"
@@ -323,20 +322,20 @@ while(True):
                            "\n3. Recognition face"
                            "\n\nEnter your choice : "))
 
-        if(choice==1):
+        if (choice == 1):
             # capturing the dataset
             capture_data(haar_cascade_path, img_dataset, database_path)
 
-        elif(choice==2):
+        elif (choice == 2):
             # train the data
             train_data(haar_cascade_path, img_dataset, trained_model)
 
-        elif(choice==3):
+        elif (choice == 3):
             # recognize/deploy the system
             recognize(haar_cascade_path, trained_model, database_path)
 
-        elif(choice==4):
-            test_accuracy()
+        # elif(choice==4):
+        #     test_accuracy()
 
         else:
             exit()
